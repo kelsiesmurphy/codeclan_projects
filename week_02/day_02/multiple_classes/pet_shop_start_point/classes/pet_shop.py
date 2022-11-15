75831ca673f9fd996d228bdb661796e6cc249b33
Kelsie Murphy <kelsie@kelsiesmurphy.com>
2022-11-15 11:27:45 +0000

    Adding completed file

--- week_02/day_02/multiple_classes/pet_shop_start_point/classes/pet_shop.py
+++ week_02/day_02/multiple_classes/pet_shop_start_point/classes/pet_shop.py
@@ -1,5 +1,33 @@
 class PetShop:
-    def __init__(self, name, pets_sold, total_cash):
+    def __init__(self, name, pets, total_cash):
         self.name = name
-        self.pets_sold = pets_sold
+        self.pets = pets
+        self.pets_sold = 0
         self.total_cash = total_cash
+
+    def stock_count(self):
+        return len(self.pets)
+
+    def increase_pets_sold(self):
+        self.pets_sold += 1
+
+    def increase_total_cash(self, cash):
+        self.total_cash += cash
+    
+    def remove_pet(self, pet):
+        self.pets.remove(pet)
+
+    def find_pet_by_name(self, name_str):
+        for pet in self.pets:
+            if pet.name == name_str:
+                return pet
+
+    def sell_pet_to_customer(self, pet_name, customer):
+        pet = self.find_pet_by_name(pet_name)
+        customer.reduce_cash(pet.price)
+        self.increase_total_cash(pet.price)
+        self.increase_pets_sold()
+        self.remove_pet(pet)
+        self.stock_count()
+        customer.add_pet(pet)
+        customer.pet_count()
