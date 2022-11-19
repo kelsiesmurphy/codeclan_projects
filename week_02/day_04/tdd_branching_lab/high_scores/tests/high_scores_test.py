ed611cfc03b0391b26deec0f8ada49b4e2b2b77d
Kelsie Murphy <kelsie@kelsiesmurphy.com>
2022-11-19 15:23:07 +0000

    Adding updates to high scores in lab

--- week_02/day_04/tdd_branching_lab/high_scores/tests/high_scores_test.py
+++ week_02/day_04/tdd_branching_lab/high_scores/tests/high_scores_test.py
@@ -8,18 +8,28 @@ from src.high_scores import latest, personal_best, personal_top_three
 class HighScoresTest(unittest.TestCase):
     
     # Tests
+    def setUp(self):
+        self.scores = [1, 3, 5, 4, 6, 7, 2]
 
     # Test latest score (the last thing in the list)
+    def test_latest_score(self):
+        self.assertEqual(2, latest(self.scores))
 
     # Test personal best (the highest score in the list)
+    def test_latest_score(self):
+        self.assertEqual(7, personal_best(self.scores))
 
     # Test top three from list of scores
 
-    # Test ordered from highest tp lowest
+
+    # Test ordered from highest to lowest
+
 
     # Test top three when there is a tie
 
+
     # Test top three when there are less than three
 
+
     # Test top three when there is only one
-    
+
