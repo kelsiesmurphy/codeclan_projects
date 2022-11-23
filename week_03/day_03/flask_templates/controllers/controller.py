7632c8f623b087446bc478e984cbc3bd7fbbee71
Kelsie Murphy <kelsie@kelsiesmurphy.com>
2022-11-23 10:46:29 +0000

    Flask work updates

--- week_03/day_03/flask_templates/controllers/controller.py
+++ week_03/day_03/flask_templates/controllers/controller.py
@@ -1,5 +1,10 @@
 from app import app
+from flask import render_template
 
 @app.route("/")
 def index():
     return "Hello World!"
+
+@app.route("/tasks")
+def show_tasks():
+    return render_template("index.html", title="Home")
---
+++ week_03/day_03/flask_templates/models/todo_list.py
@@ -0,0 +1,6 @@
+from models.task import Task
+
+task1 = Task("Cook Dinner", "Making beef stew")
+task2 = Task("Walk Dog", "Take Rufus to the park")
+
+tasks = [task1, task2]
