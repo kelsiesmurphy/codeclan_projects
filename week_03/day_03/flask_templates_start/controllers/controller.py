21a6f8572027c7d63b73d3f424cb2e22e6de64db
Kelsie Murphy <kelsie@kelsiesmurphy.com>
2022-11-23 15:30:48 +0000

    Adding changes to flask template work

--- week_03/day_03/flask_templates_start/controllers/controller.py
+++ week_03/day_03/flask_templates_start/controllers/controller.py
@@ -1,7 +1,18 @@
-from flask import render_template # ADDED
+from flask import render_template, request # ADDED
 from app import app
+from models.task import Task
 from models.todo_list import tasks
 
 @app.route('/tasks')
 def index():
     return render_template('index.html', title='Home', tasks=tasks)
+
+
+@app.route('/tasks', methods=['POST'])
+def add_task():
+    form_data = request.form
+    title = form_data["title"]
+    description = form_data["description"]
+    new_task = Task(title, description, False)
+    tasks.append(new_task)
+    return "done"
