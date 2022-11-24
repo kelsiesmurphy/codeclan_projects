5ad17b711d33a2d6398e7f7667eb47ceb7697782
Kelsie Murphy <kelsie@kelsiesmurphy.com>
2022-11-24 13:30:37 +0000

    Add pizza example in full

--- week_03/day_04/pizzas/controllers/controller.py
+++ week_03/day_04/pizzas/controllers/controller.py
@@ -1,5 +1,6 @@
-from flask import render_template
-from models.pizza_orders import pizzas
+from flask import render_template, request
+from models.pizza_orders import pizzas, get_order, add_order
+from models.pizza import Pizza
 from app import app
 
 @app.route("/")
@@ -8,4 +9,27 @@ def index():
 
 @app.route("/orders")
 def orders():
+    return render_template("orders.html", pizzas=pizzas)
+
+@app.route("/orders/<int:order_index>")
+def order(order_index):
+    my_order = get_order(order_index)
+    return render_template("order.html", pizza=my_order)
+
+
+@app.route("/orders/new")
+def new_order():
+    return render_template("new_order.html")
+
+@app.route("/orders", methods=["POST"])
+def save_order():
+    form_data = request.form
+    pizza_name = form_data["pizza_name"]
+    pizza_price = form_data["pizza_price"]
+    gluten_free = "gluten_free" in form_data
+    pizza_size = form_data["pizza_size"]
+
+    new_pizza_order = Pizza(pizza_name, pizza_price, gluten_free, pizza_size)
+    add_order(new_pizza_order)
+
     return render_template("orders.html", pizzas=pizzas)
--- week_03/day_04/pizzas/models/pizza_orders.py
+++ week_03/day_04/pizzas/models/pizza_orders.py
@@ -6,3 +6,9 @@ pizza2 = Pizza("Pepperoni", 14, False, "L")
 pizza3 = Pizza("Seafood", 12, True, "S")
 
 pizzas = [pizza1, pizza2, pizza3]
+
+def get_order(order_index):
+    return pizzas[order_index]
+
+def add_order(order):
+    pizzas.append(order)
