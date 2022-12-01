95b9ce82743bbfd3833bebd4d039234406278586
Kelsie Murphy <kelsie@kelsiesmurphy.com>
2022-12-01 11:25:24 +0000

    Adding updates to quest advisor lab

--- week_04/day_04/quest_advisor_start/db/quest_advisor.sql
+++ week_04/day_04/quest_advisor_start/db/quest_advisor.sql
@@ -1,3 +1,4 @@
+DROP TABLE visits;
 DROP TABLE users;
 DROP TABLE locations;
 
@@ -11,3 +12,11 @@ CREATE TABLE locations (
   category VARCHAR(255),
   name VARCHAR(255)
 );
+
+CREATE TABLE visits (
+  id SERIAL PRIMARY KEY,
+  user_id INT REFERENCES users(id) ON DELETE CASCADE,
+  location_id INT REFERENCES locations(id) ON DELETE CASCADE,
+  review TEXT
+);
+
