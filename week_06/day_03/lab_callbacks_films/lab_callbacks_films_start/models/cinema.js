b33715e39cf014a19f25079290acf855ffe47ba6
Kelsie Murphy <kelsie@kelsiesmurphy.com>
2022-12-14 16:28:02 +0000

    Completing Lab

--- week_06/day_03/lab_callbacks_films/lab_callbacks_films_start/models/cinema.js
+++ week_06/day_03/lab_callbacks_films/lab_callbacks_films_start/models/cinema.js
@@ -1,5 +1,50 @@
 const Cinema = function (films) {
-  this.films = films;
+    this.films = films;
 };
+
+Cinema.prototype.getFilmTitles = function() {
+    const filmTitles = this.films.map((film) => {
+        return film.title;
+    });
+    return filmTitles;
+}
+
+Cinema.prototype.findFilmByTitle = function(filmTitle) {
+    const filmByTitle = this.films.find((film) => {
+        return film.title === filmTitle;
+    })
+    return filmByTitle;
+}
+
+Cinema.prototype.filterFilmsByGenre = function(filmGenre) {
+    const filmsbyGenre = this.films.filter((film) => {
+        return filmGenre === film.genre;
+    })
+    return filmsbyGenre;
+}
+
+Cinema.prototype.checkFilmsFromYear = function(filmYear) {
+    const filmsFromYear = this.films.some((film) => {
+        return filmYear === film.year;
+    })
+    return filmsFromYear;
+}
+
+Cinema.prototype.checkFilmsOverLength = function(filmLength) {
+    const filmsOverLength = this.films.every((film) => {
+        return film.length >= filmLength;
+    })
+    return filmsOverLength;
+}
+
+Cinema.prototype.totalRunningTime = function() {
+    const totalTime = this.films.reduce((total, film) => {
+        return total + film.length;
+    }, 0)
+    return totalTime;
+}
+
+
+
 
 module.exports = Cinema;
--- week_06/day_03/lab_callbacks_films/lab_callbacks_films_start/specs/cinema_spec.js
+++ week_06/day_03/lab_callbacks_films/lab_callbacks_films_start/specs/cinema_spec.js
@@ -28,12 +28,47 @@ describe('Cinema', function () {
     assert.deepStrictEqual(actual, films);
   });
 
-  it('should be able to get a list of film titles');
-  it('should be able to find a film by title');
-  it('should be able to filter films by genre');
-  it('should be able to check whether there are some films from a particular year');
-  it('should be able to check whether there are no films from a particular year');
-  it('should be able to check whether all films are over a particular length');
-  it('should be able to calculate total running time of all films');
+  // map
+  it('should be able to get a list of film titles', function (){
+    const actual = cinema.getFilmTitles();
+    const expected = ['Moonlight', 'Blade Runner 2049', 'Dunkirk', 'Black Panther', 'T2 Trainspotting'];
+    assert.deepStrictEqual(actual, expected);
+  });
+  // filter
+  it('should be able to find a film by title', function () {
+    const actual = cinema.findFilmByTitle('Dunkirk');
+    const expected = dunkirk;
+    assert.strictEqual(actual, expected);
+  });
+  // filter
+  it('should be able to filter films by genre', function(){
+    const actual = cinema.filterFilmsByGenre('drama');
+    const expected = [moonlight, trainspotting];
+    assert.deepStrictEqual(actual, expected);
+  });
+  // filter
+  it('should be able to check whether there are some films from a particular year', function() {
+    const actual = cinema.checkFilmsFromYear(2017);
+    const expected = true;
+    assert.strictEqual(actual, expected);
+  });
+  // filter
+  it('should be able to check whether there are no films from a particular year', function() {
+    const actual = cinema.checkFilmsFromYear(2005);
+    const expected = false;
+    assert.strictEqual(actual, expected);
+  });
+  // filter
+  it('should be able to check whether all films are over a particular length', function() {
+    const actual = cinema.checkFilmsOverLength(60);
+    const expected = true;
+    assert.strictEqual(actual, expected);
+  });
+  // reduce
+  it('should be able to calculate total running time of all films', function() {
+    const actual = cinema.totalRunningTime();
+    const expected = 622;
+    assert.strictEqual(actual, expected);
+  });
 
 });
