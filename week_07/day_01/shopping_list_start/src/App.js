8fbb9884af1924b3659ceecbda181fea0957c7b8
Kelsie Murphy <kelsie@kelsiesmurphy.com>
2022-12-19 13:24:30 +0000

    Adding lab

--- week_07/day_01/shopping_list_start/src/App.js
+++ week_07/day_01/shopping_list_start/src/App.js
@@ -1,7 +1,44 @@
 import './App.css';
 import React from 'react';
+import {useState} from 'react';
 
 function App() {
+
+  const [items, setItems] = useState([
+    {name: "Bread", isPurchased: false},
+    {name: "Cheese", isPurchased: true},
+    {name: "Bow and Arrow", isPurchased: false}
+  ]);
+
+  const [newItem, setNewItem] = useState("");
+
+  const handleItemInput = (event) => {
+    setNewItem(event.target.value)
+  };
+
+  const saveNewItem = (event) => {
+    event.preventDefault();
+    const copyItems = [...items];
+    copyItems.push({name: newItem, isPurchased: false})
+    setItems(copyItems)
+    setNewItem("")
+  };
+
+  const purchaseItem = (index) => {
+    const copyItems = [...items];
+    copyItems[index].isPurchased = true;
+    // copyItems[index] = updatedItem;
+    setItems(copyItems);
+  };
+
+  const itemNodes = items.map((item, index) => {
+    return  <li key={index} className={item.isPurchased ? "purchased" : "not-purchased"}>
+              <span>{item.name}</span>
+              {item.isPurchased ? <span className="purchased">Purchased!</span> : <button onClick={() => purchaseItem(index)}>Purchase</button>}
+            </li>
+  });
+
+
   return (
     <div className="App">
 
@@ -9,9 +46,13 @@ function App() {
       <hr></hr>
 
       <ul>
+        {itemNodes}
       </ul>
 
-      <form>
+      <form onSubmit={saveNewItem}>
+        <label htmlFor="new-item">Add a new item</label>
+        <input id="new-item" type="text" value={newItem} onChange={handleItemInput}/>
+        <input type="submit" value = "Save New Item"/>
       </form>
 
     </div>
