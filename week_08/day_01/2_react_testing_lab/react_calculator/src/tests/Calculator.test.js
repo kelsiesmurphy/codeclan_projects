5570acde2b0538df3d7cba8d23eccf15ec54d39b
Kelsie Murphy <kelsie@kelsiesmurphy.com>
2023-01-09 13:52:01 +0000

    Completing Jest tests

--- week_08/day_01/2_react_testing_lab/react_calculator/src/tests/Calculator.test.js
+++ week_08/day_01/2_react_testing_lab/react_calculator/src/tests/Calculator.test.js
@@ -14,5 +14,101 @@ describe('Calculator', () => {
     fireEvent.click(button4);
     expect(runningTotal.textContent).toEqual('4');
   })
+
+  it('should add 1 to 4 and get 5', () => {
+    const number1 = container.getByTestId('number1');
+    const number4 = container.getByTestId('number4');
+    const equalsSign = container.getByTestId('operator-equals');
+    const addSign = container.getByTestId('operator-add');
+    const runningTotal = container.getByTestId('running-total');
+    fireEvent.click(number1);
+    fireEvent.click(addSign);
+    fireEvent.click(number4);
+    fireEvent.click(equalsSign);
+    expect(runningTotal.textContent).toEqual('5');
+  })
+
+  it('should subtract 4 from 7 and get 3', () => {
+    const number7 = container.getByTestId('number7');
+    const number4 = container.getByTestId('number4');
+    const equalsSign = container.getByTestId('operator-equals');
+    const subtractSign = container.getByTestId('operator-subtract');
+    const runningTotal = container.getByTestId('running-total');
+    fireEvent.click(number7);
+    fireEvent.click(subtractSign);
+    fireEvent.click(number4);
+    fireEvent.click(equalsSign);
+    expect(runningTotal.textContent).toEqual('3');
+  })
+
+  it('should multiply 3 by 5 and get 15', () => {
+    const number3 = container.getByTestId('number3');
+    const number5 = container.getByTestId('number5');
+    const equalsSign = container.getByTestId('operator-equals');
+    const multiplySign = container.getByTestId('operator-multiply');
+    const runningTotal = container.getByTestId('running-total');
+    fireEvent.click(number3);
+    fireEvent.click(multiplySign);
+    fireEvent.click(number5);
+    fireEvent.click(equalsSign);
+    expect(runningTotal.textContent).toEqual('15');
+  })
+
+  it('should divide 21 by 7 and get 3', () => {
+    const number2 = container.getByTestId('number2');
+    const number1 = container.getByTestId('number1');
+    const number7 = container.getByTestId('number7');
+    const equalsSign = container.getByTestId('operator-equals');
+    const divideSign = container.getByTestId('operator-divide');
+    const runningTotal = container.getByTestId('running-total');
+    fireEvent.click(number2);
+    fireEvent.click(number1);
+    fireEvent.click(divideSign);
+    fireEvent.click(number7);
+    fireEvent.click(equalsSign);
+    expect(runningTotal.textContent).toEqual('3');
+  })
+
+  it('should concatenate multiple number button clicks', () => {
+    const number2 = container.getByTestId('number2');
+    const runningTotal = container.getByTestId('running-total');
+    fireEvent.click(number2);
+    fireEvent.click(number2);
+    fireEvent.click(number2);
+    fireEvent.click(number2);
+    expect(runningTotal.textContent).toEqual('2222');
+  })
+
+  it('should chain multiple operations together', () => {
+    const number4 = container.getByTestId('number4');
+    const number6 = container.getByTestId('number6');
+    const number8 = container.getByTestId('number8');
+    const equalsSign = container.getByTestId('operator-equals');
+    const multiplySign = container.getByTestId('operator-multiply');
+    const subtractSign = container.getByTestId('operator-subtract');
+    const runningTotal = container.getByTestId('running-total');
+    fireEvent.click(number8);
+    fireEvent.click(subtractSign);
+    fireEvent.click(number6);
+    fireEvent.click(multiplySign);
+    fireEvent.click(number4);
+    fireEvent.click(equalsSign);
+    expect(runningTotal.textContent).toEqual('8');
+  })
+
+  it('should clear the running total without affecting the calculation', () => {
+    const clearButton = container.getByTestId('clear');
+    const number6 = container.getByTestId('number6');
+    const number8 = container.getByTestId('number8');
+    const equalsSign = container.getByTestId('operator-equals');
+    const multiplySign = container.getByTestId('operator-multiply');
+    const runningTotal = container.getByTestId('running-total');
+    fireEvent.click(number8);
+    fireEvent.click(multiplySign);
+    fireEvent.click(number6);
+    fireEvent.click(clearButton);
+    fireEvent.click(equalsSign);
+    expect(runningTotal.textContent).toEqual('0');
+  })
 })
 
