6421b0f0872ef62e7bba2e0730c50c82780f4a9e
Kelsie Murphy <kelsie@kelsiesmurphy.com>
2023-01-16 11:07:01 +0000

    Updates to server and client files

--- week_09/intro_to_express_start/client/src/App.js
+++ week_09/intro_to_express_start/client/src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <h1>Intro to Express</h1>
-  );
+
+	const [message, setMessage] = useState("")
+
+	useEffect(() => {
+		fetch('http://localhost:9000/') 
+			.then(res => res.json())
+			.then(data => setMessage(data.message))
+	})
+
+	return (
+		<>
+			<h1>Intro to Express</h1>
+			<p>The server said {message}</p>
+		</>
+	);
 }
 
 export default App;
--- week_09/intro_to_express_start/server/server.js
+++ week_09/intro_to_express_start/server/server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
+app.use(cors())
 
 app.listen(9000, function(){
     console.log("App running on port 9000");
 })
 
 app.get('/', function(req, res){
-    res.send("Hello World")
+    res.json(
+        {message: "hello world"}
+    )
 })
