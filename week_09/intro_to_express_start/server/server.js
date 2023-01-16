32050f62969baeacf657008822e0365351373d59
Kelsie Murphy <kelsie@kelsiesmurphy.com>
2023-01-16 10:13:39 +0000

    Updating server

--- week_09/intro_to_express_start/server/server.js
+++ week_09/intro_to_express_start/server/server.js
@@ -1,3 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.listen(9000, function(){
+    console.log("App running on port 9000");
+})
+
+app.get('/', function(req, res){
+    res.send("Hello World")
+})
