35510446d5ac27578809f7a09b5c71514135ff10
Kelsie Murphy <kelsie@kelsiesmurphy.com>
2023-01-17 13:28:17 +0000

    Adding completed router items

--- week_09/day_02/mongodb_with_server_games_start/server/helpers/create_router.js
+++ week_09/day_02/mongodb_with_server_games_start/server/helpers/create_router.js
@@ -1,9 +1,83 @@
 const express = require('express');
+const ObjectID = require('mongodb').ObjectID
+
+const Collection = require('mongodb/lib/collection');
 
 const createRouter = function (collection) {
 
   const router = express.Router();
 
+  // Handle index route
+  router.get('/', (req, res) => {
+    collection
+      .find()
+      .toArray()
+      .then(games => res.json(games))
+      .catch(error => {
+        console.error(error);
+        res.status(500);
+        res.json({ status: 500, error: error })
+      })
+  });
+
+  // Show (individual game) route
+  router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    collection
+      .findOne({ _id: ObjectID(id) })
+      .then(game => res.json(game))
+      .catch(error => {
+        console.error(error);
+        res.status(500);
+        res.json({ status: 500, error: error })
+      })
+  });
+
+  // Create New Game
+  router.post('/', (req, res) => {
+    const newGameData = req.body;
+    collection
+      .insertOne(newGameData)
+      .then(result => {
+        res.json(result.ops[0])
+      })
+      .catch(error => {
+        console.error(error);
+        res.status(500);
+        res.json({ status: 500, error: error })
+      })
+  });
+
+  // Delete (individual game) route
+  router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    collection
+      .deleteOne({ _id: ObjectID(id) })
+      .then(game => res.json(game))
+      .catch(error => {
+        console.error(error);
+        res.status(500);
+        res.json({ status: 500, error: error })
+      })
+  });
+
+  // Update (individual game)
+  router.put('/:id', (req, res) => {
+    const updatedGameData = req.body;
+    const id = req.params.id;
+    collection
+      .updateOne(
+        { _id: ObjectID(id) }, 
+        {$set: updatedGameData}
+      )
+      .then(game => res.json(game))
+      .catch(error => {
+        console.error(error);
+        res.status(500);
+        res.json({ status: 500, error: error })
+      })
+  });
+  
   return router;
 
 };
--- week_09/day_02/mongodb_with_server_games_start/server/server.js
+++ week_09/day_02/mongodb_with_server_games_start/server/server.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('./helpers/create_router');
+
 const app = express();
 
 app.use(express.json());
+
+MongoClient.connect('mongodb://127.0.0.1:27017', { useUnifiedTopology: true })
+  .then(client => {
+    const db = client.db('games_hub');
+    const gamesCollection = db.collection('games');
+    const gamesRouter = createRouter(gamesCollection);
+    app.use('/api/games', gamesRouter)
+  })
+  .catch(console.error);
 
 app.listen(9000, function () {
   console.log(`Listening on port ${ this.address().port }`);
