7a359f4ac587ec56b6aebc249aa65d13cb37d016
Kelsie Murphy <kelsie@kelsiesmurphy.com>
2023-01-17 15:08:51 +0000

    Adding completed homework

--- week_09/day_02/lab_full_stack_post_birds_start/server/helpers/create_router.js
+++ week_09/day_02/lab_full_stack_post_birds_start/server/helpers/create_router.js
@@ -5,6 +5,7 @@ const createRouter = function (collection) {
 
   const router = express.Router();
 
+  // Get all birds
   router.get('/', (req, res) => {
     collection
       .find()
@@ -17,11 +18,40 @@ const createRouter = function (collection) {
       });
   });
 
+  // Find one bird by ID
   router.get('/:id', (req, res) => {
     const id = req.params.id;
     collection
       .findOne({ _id: ObjectID(id) })
       .then((doc) => res.json(doc))
+      .catch((err) => {
+        console.error(err);
+        res.status(500);
+        res.json({ status: 500, error: err });
+      });
+  });
+
+  // Add new bird
+  router.post('/', (req, res) => {
+    const newBirdData = req.body
+    collection
+      .insertOne(newBirdData)
+      .then(result => {
+        res.json(result.ops[0])
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500);
+        res.json({ status: 500, error: err });
+      });
+  });
+
+  // Delete one bird by ID
+  router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    collection
+      .deleteOne({ _id: ObjectID(id) })
+      .then((result) => res.json(result))
       .catch((err) => {
         console.error(err);
         res.status(500);
